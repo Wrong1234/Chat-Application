fe3d18be877ccb97f8404aa793e9f7705f2d67d5
Wrong1234 <mahabur24cse@gmail.com>
2025-10-29 14:20:29 +0600

    project-setup

---
+++ Fronted/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex justify-center text-center">
+
+        <div className='mt-10'>
+            <p className="text-2xl ">Chat App</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
